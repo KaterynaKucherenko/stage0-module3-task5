8b0b0d1cd45737e78199476def0793432ce0874a
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-02 18:52:36 +0300

    task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,8 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
-    int x = toBeDivided/5;
+    void divide(double toBeDivided) {
+    double x = toBeDivided/5;
     System.out.println (x);
     }
 }
