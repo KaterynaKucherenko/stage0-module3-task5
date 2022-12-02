f8957eac15b23e2ac4df7bdefa3bf6b98a5c5b3d
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-02 18:48:42 +0300

    task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -4,6 +4,5 @@ public class NumberDivider {
     void divide(int toBeDivided) {
     int x = toBeDivided/5;
     System.out.println (x);
-    return x;
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,9 +1,9 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-     public static float convert(float minutes) {
+     public void convert(float minutes) {
         float x = minutes * 60.0f;
         System.out.println(x);
-        return x;
+
 }
 }
